9ea7039eb0fd863aca8cefb6e4e99f5aaaf835b2
xeswxg <51688964+xeswxg@users.noreply.github.com>
2019-09-03 10:36:54 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,10 @@
                     Laravel
                 </div>
 
+                <div class="title m-b-md">
+                    {{ date("d/m/Y H:i:s") }}
+                </div>
+                
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
