7b3622e8a19fc31c1babea27587d5b94b3671809
xeswxg <51688964+xeswxg@users.noreply.github.com>
2019-10-01 11:23:51 +0700

    Update user.php

--- resources/views/user.php
+++ resources/views/user.php
@@ -1,1 +1,10 @@
-
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Show View</title>
+  </head>
+  <body>
+         <h1>View Data :</h1>
+         <h2><?= $name ?></h2>
+  </body>
+ </html>
